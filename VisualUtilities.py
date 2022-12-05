f10e7fda824b610780c48cd8748d7f936a017635
arya hassibi <aryahassibi@gmail.com>
2022-12-05 22:36:31 +0300

    added cv2Helpmate to VisualUtilities lib to facilitate working with cv2

--- VisualUtilities.py
+++ VisualUtilities.py
@@ -1,18 +1,21 @@
 import numpy as np
 from astropy.visualization import simple_norm, astropy_mpl_style
 import matplotlib.pyplot as plt
+import cv2
 
+# A class to facilitate plotting graphs using matplotlib
 class mplPlotter:
     def __init__(self, data = np.array([])):
         plt.style.use(astropy_mpl_style)
         self.data = data
+        self.default_path = "Saved Images/"
 
         self.plot_2d = self._instance_plot_2d
         self.plot_3d = self._instance_plot_3d
     
     # Void static fucntion - takes the data as a 2d array and displayes/saves it in matplotlib
     @staticmethod
-    def plot_2d(data, filter = "jet", title = "2d Plot", display_plot = True, save_plot_as_image = True, file_name = "Saved Charts/Untitled_2d.png"):
+    def plot_2d(data, filter = "jet", title = "2d Plot", display_plot = True, save_plot_as_image = True, file_path = "Saved Images/Untitled_2d.png"):
         if(data.ndim == 2 and data.size !=0):
             plt.title(title)
             # plt.imshow(data, cmap = filter, norm = simple_norm(data, 'sqrt'))
@@ -22,7 +25,7 @@ class mplPlotter:
                 plt.show()
             
             if save_plot_as_image:
-                plt.savefig(file_name)
+                plt.savefig(file_path)
             
             plt.close()
         else:
@@ -30,7 +33,7 @@ class mplPlotter:
     
     # Void fucntion - diplays/saves the data that the class was initilized with
     # same funcitnallity as plot_2d() 
-    def _instance_plot_2d(self, filter = "jet", title = "2d Plot", display_plot = True, save_plot_as_image = True, file_name = "Saved Charts/Untitled_2d.png"):
+    def _instance_plot_2d(self, filter = "jet", title = "2d Plot", display_plot = True, save_plot_as_image = True, file_name = "Untitled_2d.png"):
         if(self.data.ndim == 2 and self.data.size != 0):
 
             plt.title(title)
@@ -40,7 +43,7 @@ class mplPlotter:
                 plt.show()
 
             if save_plot_as_image:
-                plt.savefig(file_name)
+                plt.savefig(self.default_path + file_name)
 
             plt.close()
         else:
@@ -49,7 +52,7 @@ class mplPlotter:
     # Void static function - turns the given 2d data into a 3d chart
     # if the plot_plane value is set to True it will draws a plane based on the plane_value
     @staticmethod
-    def plot_3d(data, plot_plane = False, plane_values = np.array([]), filter = "jet", title = "3d Plot", display_plot = True, save_plot_as_image = True, file_name = "Saved Charts/Untitled_23.png"):
+    def plot_3d(data, plot_plane = False, plane_values = np.array([]), filter = "jet", title = "3d Plot", display_plot = True, save_plot_as_image = True, file_path = "Saved Images/Untitled_2d.png"):
         if(data.ndim == 2 and data.size != 0):
             data_height, data_width = data.shape
             X, Y = np.meshgrid(range(data_width), range(data_height))
@@ -66,7 +69,7 @@ class mplPlotter:
                     print("Error: The given plane_values are not correct")
 
             if save_plot_as_image:
-                plt.savefig(file_name)
+                plt.savefig(file_path)
 
             if display_plot:
                 plt.show()
@@ -78,7 +81,7 @@ class mplPlotter:
     
     # Void function - turns the 2d data of the class into a 3d chart
     # same functionality as plot_3d()
-    def _instance_plot_3d(self, plot_plane = False, plane_values = np.array([]), filter = "jet", title = "3d Plot", display_plot = True, save_plot_as_image = True, file_name = "Saved Charts/Untitled_3d.png"):
+    def _instance_plot_3d(self, plot_plane = False, plane_values = np.array([]), filter = "jet", title = "3d Plot", display_plot = True, save_plot_as_image = True, file_name = "Untitled_3d.png"):
         if(self.data.ndim == 2 and self.data.size !=0):
             data_height, data_width = list(self.data.shape)
             X, Y = np.meshgrid(range(data_width), range(data_height))
@@ -95,7 +98,7 @@ class mplPlotter:
                     print("Error: The given plane_values are not correct")
             
             if save_plot_as_image:
-                plt.savefig(file_name)
+                plt.savefig(self.default_path + file_name)
 
             if display_plot:
                 plt.show()
@@ -105,3 +108,30 @@ class mplPlotter:
         else:
             print(f"Error: Number of dimensions of the the data should be 2. the # of dimentions of the given data is {self.data.ndim} and the size of it is {self.data.size}.")
     
+# A class to facilitate displaying/saving/modifying data as images using cv2
+class cv2Helpmate:
+    def __init__(self, data = np.array([])):
+        self.data = data
+
+        # values in data are mapped to values in range 0,255
+        self.mapped_data = ((self.data - np.min(self.data)) * (1 / (np.max(self.data) - np.min(self.data)) * 255)).astype('uint8')
+
+        self.image = np.stack((self.mapped_data,)*3, axis=-1)
+
+    # Void function - Displays the given 2d data as an B&W image 
+    def plot(self, window_title = "Untitled", close_window_after = 10):
+        cv2.imshow(window_title, self.image)
+        cv2.waitKey(close_window_after * 1000)
+
+    # void fucntion - saves the data as png image.
+    def save(self, file_path = "Saved Images/Untitled_cv2Image.png"):
+        cv2.imwrite(file_path, self.image)
+
+    # void fucntion - rescales the data that class was initilized with
+    def rescale(self, scale):
+        image_height, image_width = self.data.shape
+        rescaled_height, rescaled_width = int(image_height * scale), int(image_width * scale)
+
+        rescaled_image = cv2.resize(self.mapped_data, dsize = (rescaled_width, rescaled_height), interpolation = cv2.INTER_CUBIC)
+        self.data = rescaled_image
+        self.image = np.stack((self.data,)*3, axis=-1)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from DataUtilities import FitsReader
-from VisualUtilities import mplPlotter
+from VisualUtilities import mplPlotter, cv2Helpmate
 
 filename = "jw02739-o001_t001_nircam_clear-f187n_segm.fits"
 fits_data = FitsReader(filename)
 fits_data.load_image_data_from_extension(1)
 
-mpl = mplPlotter(fits_data.image_data)
-mpl.plot_2d(display_plot = False)
-mpl.plot_3d()
+# mpl = mplPlotter(fits_data.image_data)
+# mpl.plot_2d()
+# mpl.plot_3d()
+
+cvim = cv2Helpmate(fits_data.image_data)
+re = cvim.rescale(0.01)
+cvim.save()
